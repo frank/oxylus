9ec5164db9e10235ffaf3eac8b689032d8b32fb8
SoftLipsHardNips <nilstoltanso@gmail.com>
2017-12-15 15:41:47 +0100

    push

---
+++ source/csv_editor.py
@@ -0,0 +1,24 @@
+# import csv
+# readCSV = csv.reader(open('Wood_data.csv', 'rb'), delimiter=",")
+# out = csv.writer(open('data.csv', 'rb'), delimiter=",")
+# out = readCSV
+# for row in out:
+# 	out.writerow(row)
+# for row in out:
+# 	if row[8] == 'E':
+# 		row[8] = 1
+# 	if row[8] == 'M':
+# 		row[8] = 1
+# 	if row[8] == 'D':
+# 		row[8] = 1
+# 	for x in range(9):
+# 		if row[9+x] == None:
+# 			row[9+x] = False
+# 		if row[9+x] == 'X':
+# 			row[9+x] = True
+import csv
+
+with open('Wood_data.csv', 'rb') as readFile, open('data.csv', 'rb') as writeFile:
+	for row in readFile:
+		if row
+
