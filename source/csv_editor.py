c6089d32b54382e5229037a8cc4ecd051d72071e
SoftLipsHardNips <nilstoltanso@gmail.com>
2017-12-15 17:24:08 +0100

    It works boys!
    Got csv_editor to edit Wood_data.csv into data.csv

--- source/csv_editor.py
+++ source/csv_editor.py
@@ -1,25 +1,9 @@
-# import csv
-# readCSV = csv.reader(open('Wood_data.csv', 'rb'), delimiter=",")
-# out = csv.writer(open('data.csv', 'rb'), delimiter=",")
-# out = readCSV
-# for row in out:
-# 	out.writerow(row)
-# for row in out:
-# 	if row[8] == 'E':
-# 		row[8] = 1
-# 	if row[8] == 'M':
-# 		row[8] = 1
-# 	if row[8] == 'D':
-# 		row[8] = 1
-# 	for x in range(9):
-# 		if row[9+x] == None:
-# 			row[9+x] = False
-# 		if row[9+x] == 'X':
-# 			row[9+x] = True
 import csv
-
-with open('Wood_data.csv', 'rb') as readFile, open('data.csv', 'rb') as writeFile:
-	for row in readFile:
+readCSV = csv.reader(open('Wood_data.csv', 'rt'), delimiter=",")
+with open('data.csv', 'w') as writeFile:
+	writer = csv.writer(writeFile)
+	out = readCSV
+	for row in out:
 		if row[8] == 'E':
 			row[8] = 1
 		if row[8] == 'M':
@@ -27,8 +11,8 @@ with open('Wood_data.csv', 'rb') as readFile, open('data.csv', 'rb') as writeFil
 		if row[8] == 'D':
 			row[8] = 3
 		for x in range(9):
-			if row[9+x] == None:
+			if row[9+x] == '':
 				row[9+x] = False
 			if row[9+x] == 'X':
 				row[9+x] = True
-		writeFile.write(row)
+		writer.writerow(row)
